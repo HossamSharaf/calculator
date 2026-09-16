c19ff39de0762cfb23b229972a9b4451d28a7e5e
Hossam Sharaf <hossamshawky301@gmail.com>
2026-09-16 23:42:47 +0300

    feat: initialize calculator project

--- calc.cpp
+++ calc.cpp
@@ -3,5 +3,38 @@ using namespace std;
 int main()
 {
 
-        return 0;
+    while (true)
+    {
+        int number1, number2;
+        char operation;
+        cout << "please enter one of the following operations: + , - , * , / " << endl;
+        cin >> operation;
+        cout << "please enter two integers: " << endl;
+        cin >> number1 >> number2;
+        switch (operation)
+        {
+        case '+':
+            cout << number1 << " + " << number2 << " = " << number1 + number2 << endl;
+            break;
+        case '-':
+            cout << number1 << " - " << number2 << " = " << number1 - number2 << endl;
+            break;
+        case '*':
+            cout << number1 << " * " << number2 << " = " << number1 * number2 << endl;
+            break;
+        case '/':
+            cout << number1 << " / " << number2 << " = " << (double)number1 / number2 << endl;
+            break;
+        default:
+            break;
+        }
+        bool continueCalc;
+        cout << "Do you want to continue? (1 for yes, 0 for no): \n";
+        cin >> continueCalc;
+        if (!continueCalc)
+        {
+            break;
+        }
+    }
+    return 0;
 }
